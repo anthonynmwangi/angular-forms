3bcf254da03ff786ed79ea48e1ef148f16492bfc
Mwangi, Anthony Benjamin Ngaru A <mwangia@stanbic.com>
2023-07-17 11:41:10 +0300

    Retrieving data from API and populating elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +13,8 @@ import { UserSettingsFormComponent } from './user-settings-form/user-settings-fo
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -8,6 +10,8 @@ import { NgForm, NgModel } from '@angular/forms';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent {
+
+  constructor(private dataService: DataService){}
 
   originalUserSettings : UserSettings = {
     name: null,
@@ -17,6 +21,12 @@ export class UserSettingsFormComponent {
     notes: null
   }
 
+  subscriptionTypes!: Observable<string[]>;
+  // subscriptionTypes = ['one', 'two', 'three'];
+
+  postError:boolean = false;
+  postErrorMessage:string = '';
+
   userSettings: UserSettings = { ...this.originalUserSettings };
 
   onBlur(field:NgModel){
@@ -24,7 +34,27 @@ export class UserSettingsFormComponent {
     
   }
 
+  ngOnInit(){
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes() 
+  }
+
   onSubmit(form:NgForm){
     console.log('Form status: ' + form.valid);
+    if(form.valid){
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe({
+        next: result => console.log('success: ', result),
+        error: errorResponse => this.onHttpError(errorResponse)
+      });
+    }else{
+      this.postError = true;
+      this.postErrorMessage = "Please fix the above errors";
+    }
+    
+  }
+
+  onHttpError(errorResponse:any){
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
   }
 }
